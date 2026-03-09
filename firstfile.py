fc52335f1f479b1d71926293d871fec3a276ea81
paulaframz <paulaframz@gmail.com>
2026-03-08 19:44:53 -0500

    Add a success message to firstfile.py
    added a line to look for after pull request

--- firstfile.py
+++ firstfile.py
@@ -1,2 +1,3 @@
 # This is an attempt to use git and github for the first time. I am trying to create a file, add it to staging area, commit it and push it to github. I hope this works. I am using R for this. I will try to use git bash for the command line interface. I will also try to use Rstudio for the same. Let's see how it goes.
 print('this is first attempt to create a file and push to github')
+print('this seems to be working fine!')
